633fb297325f70f620dc33ab67503350b96085b6
David Levayer <david.levayer@gmail.com>
2015-03-10 22:29:50 -0400

    Adding tab concept to the home fragment

---
+++ src/main/java/com/cafejeunesse/android/common/view/ViewPagerWithHeight.java
@@ -0,0 +1,39 @@
+package com.cafejeunesse.android.common.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by David Levayer on 10/03/15.
+ */
+public class ViewPagerWithHeight extends ViewPager {
+
+    private int currentPage = 0;
+
+    public ViewPagerWithHeight(Context context) {
+        super(context);
+    }
+
+    public ViewPagerWithHeight(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
--- src/main/java/com/cafejeunesse/android/fragment/HomeFragment.java
+++ src/main/java/com/cafejeunesse/android/fragment/HomeFragment.java
@@ -4,12 +4,16 @@ import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cafejeunesse.android.common.view.SlidingTabLayout;
+import com.cafejeunesse.android.common.view.ViewPagerWithHeight;
 import com.cafejeunesse.android.filemanager.DownloadTask;
 import com.cafejeunesse.android.filemanager.NewsParser;
 import com.cafejeunesse.android.navigationdrawer.R;
@@ -34,6 +38,8 @@ public class HomeFragment extends BasicFragment implements Refreshable {
     private ViewGroup mContainerView;
     private View mView;
     private Context mContext;
+    private ViewPagerWithHeight mViewPager;
+    private SlidingTabLayout mSlidingTabLayout;
 
     private final static String CALENDAR_URL =
             "https://www.google.com/calendar/feeds/fvp8k6fun93m32q8pe972sosps%40group.calendar.google.com/public/basic";
@@ -45,6 +51,12 @@ public class HomeFragment extends BasicFragment implements Refreshable {
 
     private final static String SCHEDULE_FILEPATH = Environment.getExternalStorageDirectory().getPath()+"/horaires.xml";
 
+    private final static int CALENDAR_TAB_INDEX = 0;
+    private final static String CALENDAR_TAB_NAME = "Actualités";
+
+    private final static int SCHEDULE_TAB_INDEX = 1;
+    private final static String SCHEDULE_TAB_NAME = "Horaires";
+
     public final static long TIME_BETWEEN_DOWNLOADS = 86400000;
 
     @Override
@@ -53,13 +65,24 @@ public class HomeFragment extends BasicFragment implements Refreshable {
         mView = inflater.inflate(R.layout.fragment_home, container, false);
 
         // Chargement spécifique au fragment
-        mContainerView = (ViewGroup) mView.findViewById(R.id.news_container);
         mContext = getActivity();
+
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        mViewPager = (ViewPagerWithHeight) mView.findViewById(R.id.viewpager);
+        mViewPager.setAdapter(new SamplePagerAdapter());
+
+
+
+        mSlidingTabLayout = (SlidingTabLayout) mView.findViewById(R.id.sliding_tabs);
+        mSlidingTabLayout.setViewPager(mViewPager);
 
         testAndDownload(HomeFragment.CALENDAR_FILEPATH, HomeFragment.CALENDAR_URL);
         testAndDownload(HomeFragment.SCHEDULE_FILEPATH, HomeFragment.SCHEDULE_URL);
-
-        return mView;
     }
 
     private void testAndDownload(String filepath, String url){
@@ -86,15 +109,29 @@ public class HomeFragment extends BasicFragment implements Refreshable {
         downloadTask.execute(url,fileName);
     }
 
-    private void reloadNews(){
+    private void reloadNews(int tabIndex){
+
+        if(mContainerView == null)
+            return;
 
         mContainerView.removeAllViews();
         List<News> mNews = null;
 
         try {
-            mNews = new NewsParser().parseFileForNews(HomeFragment.CALENDAR_FILEPATH);
+
+            switch(tabIndex){
+                case CALENDAR_TAB_INDEX:
+                    mNews = new NewsParser().parseFileForNews(HomeFragment.CALENDAR_FILEPATH);
+                    break;
+                case SCHEDULE_TAB_INDEX:
+                    mNews = new NewsParser().parseFileForNews(HomeFragment.SCHEDULE_FILEPATH);
+                    break;
+                default:
+            }
+
             for (News n : mNews)
                 addNews(n);
+
         } catch (FileNotFoundException e){
             // TODO export dans un String.xml
             Toast.makeText(mContext,"Fichier XML introuvable",Toast.LENGTH_LONG).show();
@@ -117,6 +154,42 @@ public class HomeFragment extends BasicFragment implements Refreshable {
         ((TextView) newView.findViewById(R.id.home_date_day)).setText(n.getPublishingDay());
         ((TextView) newView.findViewById(R.id.home_date_month)).setText(n.getPublishingMonth());
 
+        newView.setOnTouchListener(new View.OnTouchListener() {
+
+            // TODO better way ? Can't swipe when on Title
+            private float incertitude = 20;
+            private float lastX, lastY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                int move = event.getAction();
+
+                switch(move){
+                    case MotionEvent.ACTION_DOWN:
+                        lastX = event.getX();
+                        lastY = event.getY();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        float diffX = Math.abs(lastX-event.getX());
+                        float diffY = Math.abs(lastY-event.getY());
+                        if(diffX < incertitude && diffY < incertitude){
+                            Bundle b = new Bundle();
+                            b.putString(HOME_TITLE,n.getTitle());
+                            b.putString(HOME_DESCR,n.getArticle());
+
+                            FragmentManager fm = me().getFragmentManager();
+                            HomeDialogFragment mDialogFragment = new HomeDialogFragment();
+                            mDialogFragment.setArguments(b);
+                            mDialogFragment.show(fm,"home_dialog_fragment");
+                            return true;
+                        }
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        /*
         newView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -131,6 +204,7 @@ public class HomeFragment extends BasicFragment implements Refreshable {
                 mDialogFragment.show(fm,"home_dialog_fragment");
             }
         });
+        */
 
         mContainerView.addView(newView, 0);
     }
@@ -140,6 +214,57 @@ public class HomeFragment extends BasicFragment implements Refreshable {
     }
 
     public void refresh(){
-        reloadNews();
+        int tabNumber = mViewPager.getChildCount();
+        for(int i=0; i<tabNumber; i++)
+            reloadNews(i);
     }
+
+    class SamplePagerAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return o == view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch(position){
+                case CALENDAR_TAB_INDEX:
+                    return CALENDAR_TAB_NAME;
+                case SCHEDULE_TAB_INDEX:
+                    return SCHEDULE_TAB_NAME;
+            }
+            return "noName";
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            // Inflate a new layout from our resources
+            View view = getActivity().getLayoutInflater().inflate(R.layout.fragment_home_pager,
+                    container, false);
+
+            mContainerView = (ViewGroup) view.findViewById(R.id.news_container);
+            reloadNews(position);
+
+            // Add the newly created View to the ViewPager
+            container.addView(view);
+
+            // Return the View
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+
+
+    }
+
 }
