f46a64f01c098e69b19233a2119605ebe14bdbcc
David Levayer <david.levayer@gmail.com>
2015-04-07 17:31:56 -0400

    life rules in the cafe fragment

--- app/src/main/java/com/cafejeunesse/android/fragment/cafefragment/CafeFragment.java
+++ app/src/main/java/com/cafejeunesse/android/fragment/cafefragment/CafeFragment.java
@@ -1,11 +1,15 @@
 package com.cafejeunesse.android.fragment.cafefragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.cafejeunesse.android.fragment.BasicFragment;
+import com.cafejeunesse.android.fragment.servicefragment.ServiceDetailsFragment;
 import com.cafejeunesse.android.navigationdrawer.R;
 
 /**
@@ -20,6 +24,18 @@ public class CafeFragment extends BasicFragment {
 
         // TODO ajouter un onClikListener sur le bouton afin d'afficher les regles de vie
         // On peut réutiliser le format de fragment "ServiceDetailsFragment"
+        Button b = (Button) mView.findViewById(R.id.cafe_button_rules);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                CafeLifeRulesFragment mDialogFragment = new CafeLifeRulesFragment();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.content_frame, mDialogFragment);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
 
         return mView;
     }
---
+++ app/src/main/java/com/cafejeunesse/android/fragment/cafefragment/CafeLifeRulesFragment.java
@@ -0,0 +1,22 @@
+package com.cafejeunesse.android.fragment.cafefragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cafejeunesse.android.fragment.BasicFragment;
+import com.cafejeunesse.android.navigationdrawer.R;
+
+/**
+ * Created by David Levayer on 07/04/15.
+ */
+public class CafeLifeRulesFragment extends BasicFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        initFragment(inflater,container, R.layout.cafeliferulesfragment);
+        return mView;
+    }
+}
