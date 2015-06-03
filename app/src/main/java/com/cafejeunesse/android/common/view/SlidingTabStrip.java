aef28f605a74434e52d93eb8fb2de3b1301f0508
David Levayer <david.levayer@gmail.com>
2015-06-02 20:42:49 -0400

    color adjustement: green and brown for home fragment

--- app/src/main/java/com/cafejeunesse/android/common/view/SlidingTabStrip.java
+++ app/src/main/java/com/cafejeunesse/android/common/view/SlidingTabStrip.java
@@ -31,7 +31,7 @@ class SlidingTabStrip extends LinearLayout {
     private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 2;
     private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
     private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 8;
-    private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFFFFD54F;
+    private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFF35dd17;
 
     private static final int DEFAULT_DIVIDER_THICKNESS_DIPS = 1;
     private static final byte DEFAULT_DIVIDER_COLOR_ALPHA = 0x20;
